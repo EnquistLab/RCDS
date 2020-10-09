39afe41f6cdaa065c6d41e6e7e0c4bcda39c4e7f
bmaitner <bmaitner@gmail.com>
2020-10-09 14:33:30 -0400

    new parms for maxdist and maxdistrel

--- CDS/R/CDS.R
+++ CDS/R/CDS.R
@@ -2,6 +2,8 @@
 #'
 #'Submits latitude/longitude coordinates to the centroid detection service
 #' @param coordinates Data.frame or matrix containing two columns: latitude and longitude (in that order).
+#' @param maxdist Maximum distance from centroid to qualify as centroid
+#' @param maxdistrel Maximum relative distance from centroid (relative to distance from centroid  to farthest point in political division), to qualify as a centroid
 #' @return Data.frame containing CDS output.
 #' @import RCurl
 #' @importFrom jsonlite toJSON fromJSON
@@ -14,7 +16,7 @@
 #' CDS_output <- CDS(coordinates)
 #' }
 #'
-CDS <- function(coordinates){
+CDS <- function(coordinates,maxdist=NULL,maxdistrel=NULL){
 
   # Set API options
     mode <- "resolve"										# Processing mode
@@ -25,9 +27,12 @@ CDS <- function(coordinates){
   # Convert the data to JSON
     data_json <- jsonlite::toJSON(unname(coordinates))
 
+
   # Convert the options to data frame and then JSON
     opts <- data.frame(c(mode))
     names(opts) <- c("mode")
+    if ( !is.null("maxdist") ){ opts$maxdist <- maxdist}
+    if ( !is.null("maxdistrel") ){ opts$maxdistrel <- maxdistrel}
     opts_json <-  jsonlite::toJSON(opts)
     opts_json <- gsub('\\[','',opts_json)
     opts_json <- gsub('\\]','',opts_json)
